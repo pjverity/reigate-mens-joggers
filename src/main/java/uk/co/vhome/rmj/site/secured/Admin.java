2a9d9871ef1e82908f47fc64e820f5f448f3b764
Paul Verity <paul.verity@v-home.co.uk>
2016-07-24 17:37:13 +0100

    Bare page that is to be secured

---
+++ src/main/java/uk/co/vhome/rmj/site/secured/Admin.java
@@ -0,0 +1,27 @@
+package uk.co.vhome.rmj.site.secured;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Secured administration functions
+ */
+@Controller
+@RequestMapping("/admin")
+@SuppressWarnings("unused")
+public class Admin
+{
+	private static final Logger LOGGER = LogManager.getLogger();
+
+	@RequestMapping(method = RequestMethod.GET)
+	String getAdminHome()
+	{
+		LOGGER.traceEntry();
+
+		return LOGGER.traceExit("admin");
+	}
+
+}
