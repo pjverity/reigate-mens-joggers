de12c43d2a8ea6878f86db0b69497c9a3ac05289
Paul Verity <paul.verity@v-home.co.uk>
2017-09-24 23:26:35 +0100

    reigate-mens-joggers-14: Remove site specific information from MailNotificationHandler
    Additional information about the event is supplied via a NotificationMetaDataSupplier implementation. The main application creates its own bean, so each site can provide its own information.

---
+++ src/main/java/uk/co/vhome/rmj/config/MailNotificationMetaDataSupplier.java
@@ -0,0 +1,56 @@
+package uk.co.vhome.rmj.config;
+
+import org.springframework.stereotype.Component;
+import uk.co.vhome.clubbed.notifications.BalanceUpdatedNotification;
+import uk.co.vhome.clubbed.notifications.LowBalanceNotification;
+import uk.co.vhome.clubbed.notifications.NewUserNotification;
+import uk.co.vhome.clubbed.notifications.metadatasuppliers.MailMetaData;
+import uk.co.vhome.clubbed.notifications.metadatasuppliers.NotificationMetaDataSupplier;
+
+@Component
+public class MailNotificationMetaDataSupplier implements NotificationMetaDataSupplier<MailMetaData>
+{
+	private static final String FROM_ADDRESS = "admin@reigatemensjoggers.co.uk";
+
+	private static final String FROM_NAME = "Reigate Mens Joggers";
+
+	private static final String LOW_BALANCE_NOTIFICATION_TEMPLATE = "low-balance-notification.html";
+
+	private static final String BALANCE_UPDATE_NOTIFICATION_TEMPLATE = "balance-update-notification.html";
+
+	private static final String REGISTRATION_CONFIRMATION_TEMPLATE = "registration-confirmation.html";
+
+	private static final MailMetaData LOW_BALANCE_ALERT_MAIL_META_DATA = new MailMetaData(FROM_ADDRESS,
+	                                                                                      FROM_NAME,
+	                                                                                      LOW_BALANCE_NOTIFICATION_TEMPLATE,
+	                                                                                      "Low balance alert");
+
+	private static final MailMetaData NEW_USER_MAIL_META_DATA = new MailMetaData(FROM_ADDRESS,
+	                                                                             FROM_NAME,
+	                                                                             REGISTRATION_CONFIRMATION_TEMPLATE,
+	                                                                             "Welcome to Reigate Mens Joggers!");
+
+	@Override
+	public MailMetaData metaDataFor(LowBalanceNotification notification)
+	{
+		return LOW_BALANCE_ALERT_MAIL_META_DATA;
+	}
+
+	@Override
+	public MailMetaData metaDataFor(BalanceUpdatedNotification notification)
+	{
+		String creditedOrDebited = notification.getQuantity() < 0 ? "debited" : "credited";
+
+		return new MailMetaData(FROM_ADDRESS,
+		                        FROM_NAME,
+		                        BALANCE_UPDATE_NOTIFICATION_TEMPLATE,
+		                        "Your account has been " + creditedOrDebited);
+	}
+
+	@Override
+	public MailMetaData metaDataFor(NewUserNotification notification)
+	{
+		return NEW_USER_MAIL_META_DATA;
+	}
+
+}
